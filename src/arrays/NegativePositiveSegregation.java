97d82898992013cca8a9b7eaa1eb9cb2072d83f5
Tanay Kamath <2tanaykamath@gmail.com>
2021-12-18 15:32:03 +0530

    solution: Move all the negative elements to one side of the array (4)

---
+++ src/arrays/NegativePositiveSegregation.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class NegativePositiveSegregation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        segregate(arr, n);
+        printArray(arr, n);
+    }
+
+    private static void segregate(int[] arr, int n) {
+        int start = 0, end = n - 1;
+
+        while (start <= end) {
+            if (arr[start] < 0 && arr[end] < 0) {
+                start++;
+            } else if (arr[start] > 0 && arr[end] > 0) {
+                end--;
+            } else if (arr[start] < 0 && arr[end] > 0) {
+                start++;
+                end--;
+            } else {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int pos1, int pos2) {
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+
+    private static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
