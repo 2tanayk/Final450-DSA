135727d3929bf6a1f2ae8e0c894e7fbe8e89ef64
Tanay Kamath <2tanaykamath@gmail.com>
2022-01-10 16:02:08 +0530

    solution: Best time to buy and sell stock(16)

---
+++ src/arrays/StockBuySell.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class StockBuySell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(calculateMaxProfit(arr, n));
+    }
+
+    private static int calculateMaxProfit(int[] arr, int n) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            } else if (arr[i] - min > maxProfit) {
+                maxProfit = arr[i] - min;
+            }
+        }
+        return maxProfit;
+    }
+}
