9674cb903f95d689b109c8600b97cd218da1aba7
Tanay Kamath <2tanaykamath@gmail.com>
2022-05-10 12:58:18 +0530

    solution:Intersection Point of two Linked Lists.(84)

--- src/linkedlist/IntersectionPointOfLinkedList.java
+++ src/linkedlist/IntersectionPointOfLinkedList.java
@@ -100,13 +100,4 @@ public class IntersectionPointOfLinkedList {
 
         return cur;
     }
-
-    private static void printLinkedList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-        System.out.println();
-    }
 }
