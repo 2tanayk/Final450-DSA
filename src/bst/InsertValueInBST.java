eae2977a4059efa57ce1327799852138c3c6ba01
Tanay Kamath <2tanaykamath@gmail.com>
2022-07-15 17:16:10 +0530

    solution:insert value in a BST (184)

---
+++ src/bst/InsertValueInBST.java
@@ -0,0 +1,59 @@
+package bst;
+
+import java.util.*;
+
+public class InsertValueInBST {
+    private static final Scanner sc = new Scanner(System.in);
+
+    static class Node {
+        Node left, right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = createBinaryTree();
+
+        int val = sc.nextInt();
+
+        insert(root, val);
+    }
+
+    private static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (root.data == val) {
+            return root;
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    private static Node createBinaryTree() {
+        System.out.println("Enter data:");
+        int data = sc.nextInt();
+
+        if (data == -1) return null;
+
+        Node rootNode = new Node(data);
+
+        System.out.println("Enter left value for " + data + ":");
+        rootNode.left = createBinaryTree();
+
+        System.out.println("Enter right value for " + data + ":");
+        rootNode.right = createBinaryTree();
+
+        return rootNode;
+    }
+}
