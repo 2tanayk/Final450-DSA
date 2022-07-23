6eb4f4ce9464348d33c90f66b14d16a909b3c941
Tanay Kamath <2tanaykamath@gmail.com>
2022-07-23 21:12:49 +0530

    solution:Minimum sum of two numbers formed from digits of an array (206)

--- src/bitmanipulation/NumberOfSetBits.java
+++ src/bitmanipulation/NumberOfSetBits.java
@@ -15,10 +15,8 @@ public class NumberOfSetBits {
         int ct = 0;
 
         while (n > 0) {
-            if (n % 2 == 1) {
-                ct++;
-            }
-            n /= 2;
+            n &= n - 1;
+            ct++;
         }
 
         return ct;
