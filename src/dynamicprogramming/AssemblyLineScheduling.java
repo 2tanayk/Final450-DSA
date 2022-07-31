c84c0254318c0e757f1c0248327b0af6fe1a88bd
Tanay Kamath <2tanaykamath@gmail.com>
2022-07-31 17:36:25 +0530

    solution:Assembly Line Scheduling (225)

--- src/dynamicprogramming/AssemblyLineScheduling.java
+++ src/dynamicprogramming/AssemblyLineScheduling.java
@@ -3,6 +3,7 @@ package dynamicprogramming;
 import java.util.*;
 
 public class AssemblyLineScheduling {
+    // static int[][] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -33,6 +34,17 @@ public class AssemblyLineScheduling {
     }
 
     private static int carAssembly(int n, int[][] t, int[][] s, int[] entry, int[] exit) {
+        // dp=new int[2][n];
+
+        // Arrays.fill(dp[0],-1);
+        // Arrays.fill(dp[1],-1);
+
+        //  int min=Integer.MAX_VALUE;
+
+        // for(int i=0;i<2;i++){
+        //     min=Math.min(min, entry[i] + minTime(s,i,0,t,exit));
+        // }
+
         t[0][0] += entry[0];
         t[1][0] += entry[1];
 
@@ -47,4 +59,30 @@ public class AssemblyLineScheduling {
 
         return Math.min(t[0][0], t[1][0]);
     }
+
+    // private static int minTime(int[][] s, int i, int j, int[][] t, int[] exit){
+    //     int n=s[0].length;
+
+    //     if(j==n-1){
+    //         return t[i][j] + exit[i];
+    //     }
+
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+
+    //     int min=Integer.MAX_VALUE;
+
+    //     min=Math.min(min,minTime(s,i,j+1,t,exit));
+
+    //     if(i==0){
+    //         min=Math.min(min,s[i][j+1] + minTime(s,i+1,j+1,t,exit));
+    //     }else{
+    //         min=Math.min(min,s[i][j+1] + minTime(s,i-1,j+1,t,exit));
+    //     }
+
+    //     dp[i][j]=t[i][j]+min;
+
+    //     return dp[i][j];
+    // }
 }
